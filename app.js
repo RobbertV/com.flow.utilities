69e99892bdddbdebe12df57e3c81bae97f891ff7
Robbert <mail@robbertvis.nl>
2021-10-06 22:02:26 +0200

    OPT: UX improvements for cards and texts
    # Conflicts:
    #	app.js
    #	lib/flows/actions.js
    #	locales/en.json
    #	locales/nl.json

--- app.js
+++ app.js
@@ -1,145 +1,171 @@
 "use strict";
 
 const Homey = require("homey");
-const flowActions = require('./lib/flows/actions');
-const { hoursMinutes } = require('./lib/helpers');
+const flowActions = require("./lib/flows/actions");
+const { hoursMinutes, splitTime } = require("./lib/helpers");
 
 const _settingsKey = `${Homey.manifest.id}.settings`;
 
 class App extends Homey.App {
-  log() {
-    console.log.bind(this, "[log]").apply(this, arguments);
-  }
+    log() {
+        console.log.bind(this, "[log]").apply(this, arguments);
+    }
 
-  error() {
-    console.error.bind(this, "[error]").apply(this, arguments);
-  }
+    error() {
+        console.error.bind(this, "[error]").apply(this, arguments);
+    }
 
-  // -------------------- INIT ----------------------
+    // -------------------- INIT ----------------------
 
-  async onInit() {
-    this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
+    async onInit() {
+        this.log(
+            `${this.homey.manifest.id} - ${this.homey.manifest.version} started...`
+        );
 
-    await this.initSettings();
-    await this.initTokens();
+        await this.initSettings();
+        await this.initTokens();
 
-    this.log("[onInit] - Loaded settings", this.appSettings);
+        this.log("[onInit] - Loaded settings", this.appSettings);
 
-    await flowActions.init(this.homey);
-  }
+        await flowActions.init(this.homey);
+    }
 
-  // -------------------- SETTINGS ----------------------
+    // -------------------- SETTINGS ----------------------
 
-  async initSettings() {
-    try {
-      let settingsInitialized = false;
-      this.homey.settings.getKeys().forEach((key) => {
-        if (key == _settingsKey) {
-          settingsInitialized = true;
+    async initSettings() {
+        try {
+            let settingsInitialized = false;
+            this.homey.settings.getKeys().forEach((key) => {
+                if (key == _settingsKey) {
+                    settingsInitialized = true;
+                }
+            });
+
+            this.TOKENS = {};
+
+            if (settingsInitialized) {
+                this.log("[initSettings] - Found settings key", _settingsKey);
+                this.appSettings = this.homey.settings.get(_settingsKey);
+            } else {
+                this.log(`Initializing ${_settingsKey} with defaults`);
+                await this.updateSettings({
+                    DURATIONS: [],
+                    TOTALS: [],
+                });
+            }
+        } catch (err) {
+            this.error(err);
         }
-      });
+    }
 
-      this.TOKENS = {};
+    async updateSettings(settings) {
+        try {
+            this.log("[updateSettings] - New settings:", settings);
+            this.appSettings = settings;
 
-      if (settingsInitialized) {
-        this.log("[initSettings] - Found settings key", _settingsKey);
-        this.appSettings = this.homey.settings.get(_settingsKey);
+            await this.homey.settings.set(_settingsKey, this.appSettings);
+        } catch (err) {
+            this.error(err);
+        }
+    }
 
-      } else {
-        this.log(`Initializing ${_settingsKey} with defaults`);
-        await this.updateSettings({
-          DURATIONS: [],
-          TOTALS: []
+    async initTokens() {
+        const totals = this.appSettings.TOTALS;
+        totals.forEach((t) => {
+            this.createToken(t.name, t.duration);
+
+            if (t.comparison !== null) {
+                this.createToken(
+                    `${t.name}-${this.homey.__("helpers.difference")}`,
+                    parseFloat(t.comparison),
+                    "number"
+                );
+            }
         });
-      }
-    } catch (err) {
-      this.error(err);
     }
-  }
 
-  async updateSettings(settings) {
-    try {      
-      this.log("[updateSettings] - New settings:", settings);
-      this.appSettings = settings;
-      
-      await this.homey.settings.set(_settingsKey, this.appSettings);  
-
-    } catch (err) {
-      this.error(err);
-    }
-  }
-
-  async initTokens() {
-    const totals = this.appSettings.TOTALS;
-    totals.forEach(t => {
-        this.createToken(t.name, t.duration);
-        
-        if(t.comparison !== null) {
-            this.createToken(`${t.name}-${this.homey.__("helpers.difference")}`, parseFloat(t.comparison), 'number');
+    async createToken(name, value = null, type = "string") {
+        if (!this.TOKENS[name]) {
+            this.TOKENS[name] = await this.homey.flow.createToken(name, {
+                type: type,
+                title: name,
+            });
         }
-    });
-  }
 
-  async createToken(name, value = null, type = 'string') {
-    if(!this.TOKENS[name]) {
-      this.TOKENS[name] = await this.homey.flow.createToken(name, {
-        type: type,
-        title: name
-      });
+        await this.TOKENS[name].setValue(value);
     }
 
-    await this.TOKENS[name].setValue(value);
-  }
+    // -------------------- FUNCTIONS ----------------------
 
+    async action_START_DATE(name, comparison = null) {
+        const date = new Date("2021-10-05");
+        const newSettings = this.appSettings.DURATIONS.filter(
+            (setting) => setting.name !== name
+        );
 
-// -------------------- FUNCTIONS ----------------------
+        await this.updateSettings({
+            ...this.appSettings,
+            DURATIONS: [...newSettings, { name, date, comparison }],
+        });
 
-  async action_START_DATE(name, comparison = null) {
-    const date = new Date('2021-10-05');
-    const newSettings = this.appSettings.DURATIONS.filter(setting => setting.name !== name);
-
-    await this.updateSettings({
-      ...this.appSettings,
-      DURATIONS: [...newSettings, {name, date, comparison}]
-    });
-
-    await this.createToken(name);
-    if(comparison !== null) await this.createToken(`${name}-${this.homey.__("helpers.difference")}`, parseFloat(comparison), 'number');
-  }
-  
-  async action_END_DATE(name, compare = null) {
-    const date = new Date();
-    const existing_conversion = this.appSettings.DURATIONS.find(x => x.name === name);
-
-    if(!existing_conversion) {
-      throw new Error(`No conversion start found for ${name}`);
+        await this.createToken(name);
+        if (comparison !== null)
+            await this.createToken(
+                `${name}-${this.homey.__("helpers.difference")}`,
+                parseFloat(comparison),
+                "number"
+            );
     }
 
-    const duration = await this.calculateDuration(existing_conversion.date, date);
-    const comparison = compare ? this.calculateComparison(existing_conversion.comparison, compare) : null
+    async action_END_DATE(name, compare = null) {
+        const date = new Date();
+        const existing_conversion = this.appSettings.DURATIONS.find(
+            (x) => x.name === name
+        );
 
-    const DURATIONS = this.appSettings.DURATIONS.filter(conversion => conversion.name !== name);
-    const totals = this.appSettings.TOTALS.filter(total => total.name !== name);
+        if (!existing_conversion) {
+            throw new Error(`No conversion start found for ${name}`);
+        }
 
-    await this.updateSettings({
-      ...this.appSettings,
-      DURATIONS: DURATIONS,
-      TOTALS: [...totals, {name, duration, comparison}]
-    });
+        const duration = await this.calculateDuration(
+            existing_conversion.date,
+            date
+        );
+        const comparison = compare
+            ? this.calculateComparison(existing_conversion.comparison, compare)
+            : null;
 
-    await this.createToken(name, duration);
-    if(comparison) await this.createToken(`${name}-${this.homey.__("helpers.difference")}`, parseFloat(comparison), 'number');
-  }
+        const DURATIONS = this.appSettings.DURATIONS.filter(
+            (conversion) => conversion.name !== name
+        );
+        const totals = this.appSettings.TOTALS.filter(
+            (total) => total.name !== name
+        );
 
-  calculateDuration(startDate, endDate) {   
-    const diffInMilliseconds = Math.abs(startDate - endDate) / 1000;
-    return hoursMinutes(diffInMilliseconds, this.homey.__);
-  }
+        await this.updateSettings({
+            ...this.appSettings,
+            DURATIONS: DURATIONS,
+            TOTALS: [...totals, { name, duration, comparison }],
+        });
 
-  calculateComparison(start, end) {
-      const comparison = parseFloat(end) - parseFloat(start);
-      return comparison ? comparison.toFixed(2) : 0;
-  }
+        await this.createToken(name, duration);
+        if (comparison)
+            await this.createToken(
+                `${name}-${this.homey.__("helpers.difference")}`,
+                parseFloat(comparison),
+                "number"
+            );
+    }
+
+    calculateDuration(startDate, endDate) {
+        const diffInMilliseconds = Math.abs(startDate - endDate) / 1000;
+        return splitTime(diffInMilliseconds, this.homey.__);
+    }
+
+    calculateComparison(start, end) {
+        const comparison = parseFloat(end) - parseFloat(start);
+        return comparison ? comparison.toFixed(2) : 0;
+    }
 }
 
 module.exports = App;
